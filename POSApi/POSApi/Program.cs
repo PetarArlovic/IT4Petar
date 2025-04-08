2e8c77b935b9c4ff42c0143bf400161dcc20b5e0
Petar Arlovic <petararlovic@gmail.com>
2025-04-09 00:18:20 +0200

    Added and configured the program.cs file

--- POSApi/POSApi/Program.cs
+++ POSApi/POSApi/Program.cs
@@ -1,18 +1,84 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using WEBapi1.Data;
+using WEBapi1.Interfaces;
+using WEBapi1.Mappings;
+using WEBapi1.Repositories;
+using WEBapi1.Services;
+using NLog;
+using NLog.Web;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using POSApi.Application.Services.Implementations;
+using POSApi.Application.Services.Interfaces;
+using POSApi.Infrastructure.Data;
+using POSApi.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IProizvodiService, ProizvodiService>();
+builder.Services.AddScoped<IKupciService, KupciService>();
+builder.Services.AddScoped<IStavke_racunaService, Stavke_racunaService>();
+builder.Services.AddScoped<IZaglavlje_racunaService, Zaglavlje_racunaService>();
+
+builder.Services.AddAuthorization();
+builder.Logging.ClearProviders();
+builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+builder.Host.UseNLog();
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+//   .AddEntityFrameworkStores<AppDbContext>()
+//  .AddDefaultTokenProviders();
+builder.Services.AddSwaggerGen(c =>
+{
+
+    c.EnableAnnotations();
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API Dokumentacija",
+        Version = "v1",
+        Description = "Ovo je API za moj projekt",
+        Contact = new OpenApiContact
+        {
+            Name = "Petar Arlovic",
+            Email = "petararlovic@gmail.com",
+        }
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+
+});
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+
+    string connectionString = builder.Configuration.GetConnectionString("WebDB1")!;
+    options.UseSqlServer(connectionString);
+
+});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 }
+
+// Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
