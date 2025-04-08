37389a8f0748f8f4edc3b94c1f824352f05a1dda
Petar Arlovic <petararlovic@gmail.com>
2025-04-08 23:48:32 +0200

    Added Models
    Added and configured the models

--- POSApi/POSApi/Domain/Models/Kupac.cs
+++ POSApi/POSApi/Domain/Models/Kupac.cs
@@ -1,6 +1,29 @@
-﻿namespace POSApi.Domain.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace POSApi.Domain.Models
 {
     public class Kupac
     {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int SIFRA { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string NAZIV { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string ADRESA { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string MJESTO { get; set; }
+
+        public ICollection<Zaglavlje_racuna> ZAGLAVLJE_RACUNA { get; set; }
+
     }
 }
--- POSApi/POSApi/Domain/Models/Zaglavlje_racuna.cs
+++ POSApi/POSApi/Domain/Models/Zaglavlje_racuna.cs
@@ -1,6 +1,29 @@
-﻿namespace POSApi.Domain.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace POSApi.Domain.Models
 {
     public class Zaglavlje_racuna
     {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int BROJ { get; set; }
+
+        [Required]
+        public DateTime DATUM { get; set; } = DateTime.Now;
+
+        [MaxLength(100)]
+        public string NAPOMENA { get; set; }
+
+        [ForeignKey("KUPAC")]
+        public int KUPACId { get; set; }
+
+        public Kupac KUPAC { get; set; }
+
+        public ICollection<Stavke_racuna> STAVKE_RACUNA { get; set; }
+
     }
 }
