6fa5dfda0061c2fbc67c306755e899da9835251c
Petar Arlovic <petararlovic@gmail.com>
2025-04-08 23:53:29 +0200

    Deleted extra files

--- POSApi/POSApi/Domain/Interfaces/IProizvodiService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace POSApi.Domain.Interfaces
-{
-    public interface IProizvodiService
-    {
-    }
-}
--- POSApi/POSApi/Domain/Interfaces/IStavke_racunaService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace POSApi.Domain.Interfaces
-{
-    public interface IStavke_racunaService
-    {
-    }
-}
