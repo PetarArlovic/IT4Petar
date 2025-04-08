7796d7df4a4943ed39c88d16faed143ffea224ee
Petar Arlovic <petararlovic@gmail.com>
2025-04-09 00:13:26 +0200

    Added and configured the AppDbContext file

---
+++ POSApi/POSApi/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POSApi.Domain.Models;
+
+namespace POSApi.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //Configuring DB tables
+        public DbSet<Kupac> KUPAC { get; set; }
+        public DbSet<Proizvod> PROIZVOD { get; set; }
+        public DbSet<Zaglavlje_racuna> ZAGLAVLJE_RACUNA { get; set; }
+        public DbSet<Stavke_racuna> STAVKE_RACUNA { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Kupac>()
+                .HasIndex(k => k.SIFRA)
+                .IsUnique();
+
+            modelBuilder.Entity<Proizvod>()
+                .HasIndex(p => p.SIFRA)
+                .IsUnique();
+
+            modelBuilder.Entity<Zaglavlje_racuna>()
+                .HasIndex(z => z.BROJ)
+                .IsUnique();
+
+            modelBuilder.Entity<Zaglavlje_racuna>()
+                .HasOne(z => z.KUPAC)
+                .WithMany(k => k.ZAGLAVLJE_RACUNA)
+                .HasForeignKey(z => z.KUPACId);
+
+            modelBuilder.Entity<Stavke_racuna>()
+                .HasOne(s => s.PROIZVOD)
+                .WithMany(p => p.STAVKE_RACUNA)
+                .HasForeignKey(s => s.PROIZVODId);
+
+            modelBuilder.Entity<Stavke_racuna>()
+                .HasOne(s => s.ZAGLAVLJE_RACUNA)
+                .WithMany(z => z.STAVKE_RACUNA)
+                .HasForeignKey(s => s.ZAGLAVLJE_RACUNAId);
+
+        }
+    }
+}
