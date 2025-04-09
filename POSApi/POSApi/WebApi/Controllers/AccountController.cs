ccab302782e7cbfd46f886eb826270aab4a0f024
Petar Arlovic <petararlovic@gmail.com>
2025-04-09 15:34:19 +0200

    Configured the AccountController
    Also fixed a little bug in the json file
    Tested the JWT token

--- POSApi/POSApi/WebApi/Controllers/AccountController.cs
+++ POSApi/POSApi/WebApi/Controllers/AccountController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using POSApi.Domain.Models;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 
 namespace POSApi.WebApi.Controllers
 {
@@ -10,13 +13,23 @@ namespace POSApi.WebApi.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly IConfiguration configuration;
 
         public AccountController(IConfiguration configuration)
         {
-            
-
-
+            this.configuration = configuration;
         }
+
+        /*
+        [HttpGet("TestToken")]
+        public IActionResult TestToken()
+        {
+            User user = new User() {Id = 2, ROLE = "admin"};
+            string jwt = CreateJWToken(user);
+            var response = new { JWToken = jwt };
+            return Ok(response);
+        }
+        */
 
         private string CreateJWToken(User user)
         {
@@ -28,6 +41,33 @@ namespace POSApi.WebApi.Controllers
                 new Claim ("role", user.ROLE),
 
             };
+
+            string strKey = configuration["Jwt:Key"]!;
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(strKey));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["Jwt:Issuer"],
+                audience: configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
         }
     }
 }
+
+
+
+
+// ||
+// {}
+//  <>
+//  []
+
