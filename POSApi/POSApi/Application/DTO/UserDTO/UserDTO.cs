31e7b55761e8e819a3fe999352832890e6edd36c
Petar Arlovic <petararlovic@gmail.com>
2025-04-09 15:55:00 +0200

    Added and configured the UserDTO files

---
+++ POSApi/POSApi/Application/DTO/UserDTO/UserDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace POSApi.Application.DTO.UserDTO
+{
+    public class UserDTO
+    {
+
+        [Required]
+        [MaxLength(100)]
+        public string NAZIV { get; set; } = "";
+
+        [Required]
+        [MaxLength(100)]
+        public string EMAIL { get; set; } = "";
+
+        [Required]
+        [MaxLength(100), MinLength(8)]
+        public string PASSWORD { get; set; } = "";
+
+    }
+}
---
+++ POSApi/POSApi/Application/DTO/UserDTO/UserProfileDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace POSApi.Application.DTO.UserDTO
+{
+    public class UserProfileDTO
+    {
+
+        public int Id { get; set; }
+
+        public string NAZIV { get; set; } = "";
+
+        public string EMAIL { get; set; } = "";
+
+        public string ROLE { get; set; } = "";
+
+        public DateTime CREATED_AT { get; set; }
+
+    }
+}
