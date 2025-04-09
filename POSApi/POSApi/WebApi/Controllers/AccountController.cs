5845ab7a55be8ebd31f5bf8b85bb7c809fc584e4
Petar Arlovic <petararlovic@gmail.com>
2025-04-09 15:13:47 +0200

    Configured appsettings
    For the JWT and made an AccountController

---
+++ POSApi/POSApi/WebApi/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using POSApi.Domain.Models;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace POSApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        public AccountController(IConfiguration configuration)
+        {
+            
+
+
+        }
+
+        private string CreateJWToken(User user)
+        {
+
+            List<Claim> claims = new List<Claim>
+            {
+
+                new Claim ("id", "" + user.Id),
+                new Claim ("role", user.ROLE),
+
+            };
+        }
+    }
+}
