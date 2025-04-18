95e2c874e014d7d71b4b7793bb8e8a0620d2e1c9
Petar Arlovic <petararlovic@gmail.com>
2025-04-18 12:09:05 +0200

    Commit to start merge

--- POSApi/POSApi/Client/src/app/guards/auth.guard.ts
+++ POSApi/POSApi/Client/src/app/guards/auth.guard.ts
@@ -1,10 +1,12 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  if (sessionStorage.getItem('email')) {
+export const authGuard: CanActivateFn = () => {
+  const router = inject(Router);
+
+  if (sessionStorage.getItem('token')) {
     return true;
-  }else{}
-  const router = inject(Router)
-  return router.navigate(['login']);
+  }
+
+  return router.parseUrl('/login');
 };
